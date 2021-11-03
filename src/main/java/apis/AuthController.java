1ad75bfeaa96c7185c98f1e386b1a0cadd758aa5
Merloin <tahirdovtalabi@gmail.com>
2021-11-03 23:50:34 +0400

    v1

--- src/main/java/apis/AuthController.java
+++ src/main/java/apis/AuthController.java
@@ -1,2 +1,15 @@
-package apis;public class AuthController {
+package apis;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthController {
+
+    public String hail (String username) {
+        return String.format("Hello, /s", username);
+    }
+
+    public String farewell (String username) {
+        return String.format("See you later, /s", username);
+    }
 }
--- src/main/java/repos/AuthRepo.java
+++ src/main/java/repos/AuthRepo.java
@@ -1,2 +1,18 @@
-package repos;public class AuthRepo {
+package repos;
+
+import apis.AuthController;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AuthRepo {
+    private final AuthController auth;
+
+    public AuthRepo(AuthController auth) {
+        this.auth = auth;
+    }
+
+    private void doThing (String username) {
+        auth.hail(username);
+    }
+
 }
